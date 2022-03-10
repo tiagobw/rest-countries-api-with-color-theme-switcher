e17970b064935c3beb94a99f38a701d48b30e6ca
Tiago <tiago@Tiagos-MacBook-Air.local>
2022-03-10 05:50:06 -0300

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      'dark-blue': 'hsl(209, 23%, 22%)',
+      'very-dark-blue-background': 'hsl(207, 26%, 17%)',
+      'very-dark-blue-text': 'hsl(200, 15%, 8%)',
+      'dark-gray-input': 'hsl(0, 0%, 52%)',
+      'very-light-gray-background': 'hsl(0, 0%, 98%)',
+      'white-text-elements': 'hsl(0, 0%, 100%)',
+    },
   },
   plugins: [],
 };
