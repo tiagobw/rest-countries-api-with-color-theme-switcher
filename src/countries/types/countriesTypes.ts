2bb8e580cbcde50846ca3a542ebfe8fa7981a19c
Tiago <tiago@Tiagos-MacBook-Air.local>
2022-03-10 05:08:57 -0300

    complete CountryType

--- src/countries/types/countriesTypes.ts
+++ src/countries/types/countriesTypes.ts
@@ -1,8 +1,29 @@
 export type CountryType = {
   name: {
-    common: 'string';
-    official: 'string';
+    common: string;
+    official: string;
+    nativeName: {
+      [key: string]: {
+        official: string;
+        common: string;
+      };
+    };
   };
+  currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+  languages: {
+    [key: string]: string;
+  };
+  borders: string[];
+  population: number;
+  region: string;
+  subregion: string;
+  capital: string[];
+  tld: string[];
 };
 
 export interface CountriesFetcher {
