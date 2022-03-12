60f26c97718b6764d654982c9dc22c06481eec36
Tiago <tiago@Tiagos-MacBook-Air.local>
2022-03-11 22:17:17 -0300

    finish header desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='bg-white-text-elements h-28 flex justify-between items-center drop-shadow-md px-7'>
-      <h1 className='font-extrabold text-xl cursor-pointer'>
+    <header
+      className={`bg-white-text-elements flex justify-between 
+                    items-center h-28 drop-shadow-md px-7 
+                    md:px-20 md:h-24`}
+    >
+      <h1 className='font-extrabold text-xl cursor-pointer md:text-2xl'>
         <Link to='/'>Where in the world?</Link>
       </h1>
       <div className='flex items-center cursor-pointer'>
